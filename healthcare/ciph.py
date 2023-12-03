290c08caf3d014d6a8a722b57ccfc707b5531f9b
Swapnil Guragain <swapnilguragain@Swapnils-MBP.lan>
2023-12-02 22:49:53 -0500

    add new Patients and noise added to age

---
+++ healthcare/ciph.py
@@ -0,0 +1,33 @@
+import random
+from datetime import date
+
+
+
+def rand_key():
+	key = ""
+	for i in range(2):
+		temp = str(random.randint(0, 1))
+		key += temp
+	return(key)
+
+def getAge(dob):
+	today = date.today()
+	year, month, day = map(int, dob.split('-'))
+	dob = date(year, month, day)
+	age = today.year - dob.year - ((today.month, today.day)<(dob.month, dob.day))
+	return age
+	
+def getGenderBin(gender):
+	if gender == 'male' or gender == 'Male' or gender == 'MALE' or gender == 'm' or gender == 'M':
+		gender = '1'
+	else:
+		gender = '0'
+	gender = rand_key() + gender + rand_key()
+	return gender
+
+
+def getGender(gender):
+	line = gender
+	for i in range(2, len(line), 3):
+		gender = line[i]
+	return gender
--- healthcare/views.py
+++ healthcare/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Patient
+from .ciph import getAge, getGender, getGenderBin
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from datetime import datetime
 
 def home(request):
 	return render(request, 'home.html', {})
@@ -32,6 +34,18 @@ def logout_user(request):
 	return redirect('home')
 
 def addPatients(request):
+	if request.method == "POST":
+		firstName = request.POST.get('firstname')
+		lastName = request.POST.get('lastname')
+		age = getAge(request.POST.get('dob'))
+		gender = getGenderBin(request.POST.get('gender'))
+		weight = request.POST.get('weight')
+		height = request.POST.get('height')
+		healthHistory = request.POST.get('helhis')
+
+		newPatient = Patient(firstName = firstName, lastName = lastName, age = age, gender = gender, weight = weight, height = height, healthHistory = healthHistory)
+		newPatient.save()
+
 	return render(request, 'addPatients.html', {})
 
 
